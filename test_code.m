94c3f2049fe1a7d0c6026af6b7962cc2b9128e98
Julie Lafaye <julie.lafaye07@gmail.com>
2019-01-17 18:29:11 +0100

    correction angle code

--- test_code.m
+++ test_code.m
@@ -1,19 +1,39 @@
-figure;
 
-f = 2;
-angle = ang_body(f,1:end-1);
+f = 7;
+% ----- Correct head-tail detection
+angle_r = ang_body(f,:);
+plot(ang_body(f,:)*180/pi);
+for i = 2:nb_frame-1
+    diff_g = ang_body(f,i) - ang_body(f,i-1);
+    diff_d = ang_body(f,i+1) - ang_body(f,i);
+    t = diff_g - diff_d;
+    if abs(diff_g) > 150*pi/180 && abs(diff_d) > 150*pi/180
+        
+        if abs(t) > 5
+            angle_r(i) = (angle_r(i-1) + angle_r(i+1))/2;
+        end
+    end
+    
+end
+hold on
+plot(angle_r*180/pi)
+
+
+
+figure
+angle = angle_r;
 % for f = 1:nb_detected_object
     for i = 2:nb_frame
         
-        d = (ang_body(f,i) - ang_body(f,i-1))*180/pi;
+        d(i) = (angle_r(i) - angle_r(i-1))*180/pi;
         
         if isnan(d) == 0
-            t = angle_per_frame(d)*pi/180;
+            t(i) = angle_per_frame(d(i))*pi/180;
            
-%             if abs(t) < 160*pi/180
-                angle(1,i) = angle(1,i-1) + t ;
+%             if abs(t) < 170*pi/180
+                angle(1,i) = angle(1,i-1) + t(i);
 %             else
-%                 angle(f,i) = angle(f,i-1);
+%                 angle(1,i) = angle(1,i-1);
 %             end
         end
         
@@ -23,3 +43,4 @@ angle = ang_body(f,1:end-1);
     plot(angle(1,:)*180/pi)
 % end
 plot([1 345],[360 360],'k')
+

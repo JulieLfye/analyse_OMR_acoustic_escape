8cb0944264e12796586c1f0d6a1de560da9e5e61
Julie <julie.lafaye07@gmail.com>
2019-01-28 17:36:45 +0100

    try to create a new correct_angle

--- test_code.m
+++ test_code.m
@@ -2,13 +2,62 @@
 
 % save(fullfile(a,'angle_to_OMR.mat'),angle_to_OMR)
 
-clc
+% clc
+%
+% F = Focus();
+%
+% F.dpf = 5;
+% F.OMR = 1000;
+% F.V = '1_0';
+%
+% Data = F.load('');
 
-F = Focus();
+%% -------------------------------------------------
 
-F.dpf = 5;
-F.OMR = 1000;
-F.V = 1;
+ang = ang_body(1,:);
+ang1 = ang;
+close all
+% correct angle ie 180° problem
+% d = [nan abs(diff(ang))];
+d = [nan diff(ang)];
+plot(d)
+[val,ind,w,p] = findpeaks(d,'MinPeakHeight',110*pi/180);
+% [val,ind] = findpeaks(d,'MinPeakDistance',2);
+hold on
+plot(ind,val,'o')
 
-Data = F.load('data.m')
-    
+for i = 1:size(ind,2)
+    n = round(w(i));
+    if n > 1 && p(i) > 1
+        if n == 2
+            dg = abs(d(ind(i))-d(ind(i)-n/2));
+            dd = abs(d(ind(i)+n/2)-d(ind(i)));
+            if dd <= 0.4  %17.2°, ind(i) is the angle to change
+                q = ind(i);
+                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
+            end
+            if dg <= 0.4 % here it's ind(i)-1
+                q = ind(i)-1;
+                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
+            end
+        end
+        if n == 4
+            dg = abs(d(ind(i))-d(ind(i)-n/2));
+            dd = abs(d(ind(i)+n/2)-d(ind(i)));
+            if dd <= 0.4  %17.2°, ind(i) is the middle
+                q = ind(i);
+                ang1(q-1) = (ang1((q-1)-2)+ang1((q-1)-1))/2;
+                ang1(q+1) = (ang1((q+1)-2)+ang1((q+1)-1))/2;
+                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
+            end
+            if dg <= 0.4 %  ind(i)-1 is the middle
+                q = ind(i)-1;
+                ang1(q-1) = (ang1((q-1)-2)+ang1((q-1)-1))/2;
+                ang1(q+1) = (ang1((q+1)-2)+ang1((q+1)-1))/2;
+                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
+            end
+        end
+    end
+end
+figure;
+plot(ang1)

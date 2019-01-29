bc107b527cf1470774e4d414d3d61ddbd1918939
Julie <julie.lafaye07@gmail.com>
2019-01-29 12:09:34 +0100

    try2 correct_angle

--- test_code.m
+++ test_code.m
@@ -13,51 +13,74 @@
 % Data = F.load('');
 
 %% -------------------------------------------------
-
-ang = ang_body(1,:);
-ang1 = ang;
 close all
+f = 8;
+ang1 = ang_body(f,:);
+figure;
+plot(ang1);
+b = 0.4;
+% for f = 1:nb_detected_object
+
 % correct angle ie 180° problem
 % d = [nan abs(diff(ang))];
-d = [nan diff(ang)];
+d = [nan diff(ang_body(f,:))];
 plot(d)
-[val,ind,w,p] = findpeaks(d,'MinPeakHeight',110*pi/180);
-% [val,ind] = findpeaks(d,'MinPeakDistance',2);
+[val,ind] = findpeaks(d,'MinPeakHeight',pi/2);
 hold on
 plot(ind,val,'o')
-
+f = 1;
 for i = 1:size(ind,2)
-    n = round(w(i));
-    if n > 1 && p(i) > 1
-        if n == 2
-            dg = abs(d(ind(i))-d(ind(i)-n/2));
-            dd = abs(d(ind(i)+n/2)-d(ind(i)));
-            if dd <= 0.4  %17.2°, ind(i) is the angle to change
-                q = ind(i);
-                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
-            end
-            if dg <= 0.4 % here it's ind(i)-1
-                q = ind(i)-1;
-                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
-            end
+    q = ind(i);
+    dg = d(q-1);
+    dd = d(q+1);
+    if abs(dg) <= b && abs(dd) <= b
+        dgg = d(q-2);
+        ddd = d(q+2);
+        if abs(dgg) <= b && abs(ddd) <= b
+            ang1(f,q) = ang1(f,q);
+        elseif abs(dgg) <= b
+            ang1(f,q) = ang1(f,q-2);
+            ang1(f,q+1) = ang1(f,q-1);
+        elseif abs(ddd) <= b
+            ang1(f,q-1) = ang1(f,q-3);
+            ang1(f,q) = ang1(f,q-2);
         end
-        if n == 4
-            dg = abs(d(ind(i))-d(ind(i)-n/2));
-            dd = abs(d(ind(i)+n/2)-d(ind(i)));
-            if dd <= 0.4  %17.2°, ind(i) is the middle
-                q = ind(i);
-                ang1(q-1) = (ang1((q-1)-2)+ang1((q-1)-1))/2;
-                ang1(q+1) = (ang1((q+1)-2)+ang1((q+1)-1))/2;
-                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
-            end
-            if dg <= 0.4 %  ind(i)-1 is the middle
-                q = ind(i)-1;
-                ang1(q-1) = (ang1((q-1)-2)+ang1((q-1)-1))/2;
-                ang1(q+1) = (ang1((q+1)-2)+ang1((q+1)-1))/2;
-                ang1(q) = (ang1(q-2)+ang1(q-1))/2;
-            end
+    elseif abs(dg) <= b
+        ang1(f,q) = ang1(f,q-1);
+    elseif abs(dd) <= b
+        ang1(f,q-1) = ang1(f,q-2);
+    elseif dg < -b && dd < -b
+        ang1(f,q) = ang1(f,q-1);
+    end
+end
+% end
+% figure;
+% plot(ang_body(10,:));
+figure;
+plot(ang1');
+
+
+angle = ang1;
+ang_OMR = ang1;
+% for f = 1:nb_detected_object
+for i = 2:nb_frame
+    d = (ang1(f,i) - ang1(f,i-1))*180/pi;
+    if isnan(d) == 0
+        ta(i) = angle_per_frame(d);
+        if abs(ta(i)) <= 100
+            angle(f,i) = angle(f,i-1) + ta(i);
+        else
+            angle(f,i) = angle(f,i-1);
         end
     end
 end
-figure;
-plot(ang1)
+angle(f,end) = angle(f,end-1);
+
+OMRangle = mod(P.OMR.angle,360)*pi/180;
+ang_OMR(f,:) = angle(f,:) - OMRangle;
+ang2 = mean(angle(f,1:5),'omitnan');
+if ang2 > pi
+    ang_OMR(f,:) = angle(f,:) - 2*pi;
+end
+% end
+figure, plot(ang_OMR');

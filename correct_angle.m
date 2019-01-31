83e2ceb1257ceffff6f5120d510df1e38ff99836
Julie Lafaye <julie.lafaye07@gmail.com>
2019-01-31 18:08:51 +0100

    update 31/01 still some problem with angle
    look 19-01-15 1Vpp run10

--- correct_angle.m
+++ correct_angle.m
@@ -1,140 +1,184 @@
-function [angle, ang_OMR] = correct_angle(nb_detected_object,...
-    nb_frame, ang_body, fig, P)
+% function [angle, ang_OMR] = correct_angle(nb_detected_object,...
+%     nb_frame, ang_body, fig, P)
 
 ang = ang_body;
-
-ang = ang_body(f,:);
-
-d  = [nan diff(ang)];
-plot(d)
-[val,ind] = findpeaks(d,'MinPeakHeight',pi/2);
-% b = find(val>4.5);
-% val(b) = [];
-% ind(b) = [];
-hold on
-plot(ind,val,'o')
-
-
-
-%% determine group of peaks to correct
-dind = diff(ind);
-b1 = find(dind<8);
-a = diff(b1);
-b2 = find(a>1);
-ind2 = ind;
-if isempty(b2) == 0
-    n1 = 1;
-    n2 = b2(1);
-    i = 1;
-    
-    while i <= size(b2,2)+1
-        if n1 == n2
-            gpind = ind(b1(n1):b1(n1)+1);
-            ind2(b1(n1):b1(n1)+1) = nan;
-            i = i+1;
-            n1 = n2+1;
-            if i >= size(b2,2)+1
-                n2 = size(b1,2);
-            else
-                n2 = b2(i);
-            end
-            
-            % correct the angle now
-            mmin = min(gpind);
-            mmax = max(gpind);
-            m = round(mean(gpind));
-            md = mean(ang(mmax+4:mmax+5),'omitnan');
-            mg = mean(ang(mmin-5:mmin-4),'omitnan');
-            % correction
-            for j = mmin-3:m
-                ang(j) = mg;
-            end
-            for j = m+1:mmax+3
-                ang(j) = md;
-            end
-            
-            
-        else
-            gpind = ind(b1(n1):b1(n2)+1);
-            ind2(b1(n1):b1(n2)+1) = nan;
-            i = i+1;
-            n1 = n2+1;
-            if i >= size(b2,2)+1
-                n2 = size(b1,2);
-            else
-                n2 = b2(i);
-            end
-            
-            % correct the angle now
-            mmin = min(gpind);
-            mmax = max(gpind);
-            m = round(mean(gpind));
-            md = mean(ang(mmax+4:mmax+5),'omitnan');
-            mg = mean(ang(mmin-5:mmin-4),'omitnan');
-            % correction
-            for j = mmin-3:m
-                ang(j) = mg;
-            end
-            for j = m+1:mmax+3
-                ang(j) = md;
-            end
-        end
-    end
-elseif isempty(b1) == 0
-    n1 = 1;
-    gpind = ind(b1(n1):b1(n1)+1);
-    mmin = min(gpind);
-    mmax = max(gpind);
-    m = round(mean(gpind));
-    md = mean(ang(mmax+4:mmax+5),'omitnan');
-    mg = mean(ang(mmin-5:mmin-4),'omitnan');
-    for j = mmin-3:m
-        ang(j) = mg;
-    end
-    for j = m+1:mmax+3
-        ang(j) = md;
-    end
-    
-end
-
-
-%% correct isolated point
-
-l = find(isnan(ind2) == 0);
-for i = 1:size(l,2)
-    q = ind2(l(i));
-    md = mean(ang(q+4:q+5),'omitnan');
-    mg = mean(ang(q-5:q-4),'omitnan');
-    % correct angle
-    ang(q-2) = mg;
-    ang(q-1) = mg;
-    ang(q) = mg;
-    ang(q+1) = md;
-    ang(q+2) = md;
-    
-end
-
-ang(1) = mean(ang(2:5),'omitnan');
-ang(end) = mean(ang(end-5:end-2),'omitnan');
-figure;
-plot(ang_body(f,:));
-hold on;
-plot(ang);
-
-%% Correction of the 0-360 edge
 angle = ang;
+ang_OMR = angle;
+OMRangle = mod(P.OMR.angle,360)*pi/180;
+
 % for f = 1:nb_detected_object
-for i = 2:nb_frame
-    d = (ang(i) - ang(i-1))*180/pi;
-    if isnan(d) == 0
-        ta = angle_per_frame(d);
-        if abs(ta) <= 150
-            angle(i) = angle(i-1) + ta*pi/180;
-        else
-            angle(i) = angle(i-1);
+    
+    f = 5;
+    
+    d  = [nan diff(ang(f,:))];
+    [val,ind] = findpeaks(d,'MinPeakHeight',pi/2);
+%         plot(d)
+%         hold on
+%         plot(ind,val,'o')
+    
+    %% determine  ang correct angle for group of peaks
+    dind = diff(ind);
+    b1 = find(dind<8);
+    a = diff(b1);
+    b2 = find(a>1);
+    ind2 = ind;
+    if isempty(b2) == 0
+        n1 = 1;
+        n2 = b2(1);
+        i = 1;
+        while i <= size(b2,2)+1 % 2 groups at least
+            %%
+            if n1 == n2
+                %create group of index
+                gpind = ind(b1(n1):b1(n1)+1);
+                ind2(b1(n1):b1(n1)+1) = nan;
+                i = i+1;
+                n1 = n2+1;
+                if i >= size(b2,2)+1
+                    n2 = size(b1,2);
+                else
+                    n2 = b2(i);
+                end
+                % correct the angle
+                mmin = min(gpind);
+                mmax = max(gpind);
+                m = round(mean(gpind));
+                if mmax <= nb_frame-5 && mmin-5 >= 1
+                    md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+                    mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+                elseif mmax > nb_frame-5
+                    md = ang(f,end);
+                    mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+                elseif mmin <= 5
+                    mg = ang(f,1);
+                    md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+                end
+                % correction
+                if mmax <= nb_frame-3 && mmin-3 >= 1
+                    ang(f,mmin-3:m-1) = mg;
+                    ang(f,m:mmax+3) = md;
+                elseif mmax > nb_frame-3
+                    ang(f,m:end ) = md;
+                elseif mmin <= 3
+                    ang(f,1:m-1) = mg;
+                end
+                %%
+            else
+                % create group of index
+                gpind = ind(b1(n1):b1(n2)+1);
+                ind2(b1(n1):b1(n2)+1) = nan;
+                i = i+1;
+                n1 = n2+1;
+                if i >= size(b2,2)+1
+                    n2 = size(b1,2);
+                else
+                    n2 = b2(i);
+                end
+                % correct the angle
+                mmin = min(gpind);
+                mmax = max(gpind);
+                m = round(mean(gpind));
+                if mmax <= nb_frame-5 && mmin-5 >= 1
+                    md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+                    mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+                elseif mmax > nb_frame-5
+                    md = ang(f,end);
+                    mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+                elseif mmin <= 5
+                    mg = ang(f,1);
+                    md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+                end
+                % correction
+                if mmax <= nb_frame-3 && mmin-3 >= 1
+                    ang(f,mmin-3:m-1) = mg;
+                    ang(f,m:mmax+3) = md;
+                elseif mmax > nb_frame-3
+                    ang(f,m:end ) = md;
+                elseif mmin <= 3
+                    ang(f,1:m-1) = mg;
+                end
+            end
+        end
+    elseif isempty(b1) == 0 %only one group
+        n1 = 1;
+        gpind = ind(b1(n1):b1(n1)+1);
+        mmin = min(gpind);
+        mmax = max(gpind);
+        m = round(mean(gpind));
+        if mmax <= nb_frame-5 && mmin-5 >= 1
+            md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+            mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+        elseif mmax > nb_frame-5
+            md = ang(f,end);
+            mg = mean(ang(f,mmin-5:mmin-4),'omitnan');
+        elseif mmin <= 5
+            mg = ang(f,1);
+            md = mean(ang(f,mmax+4:mmax+5),'omitnan');
+        end
+        % correction
+        if mmax <= nb_frame-3 && mmin-3 >= 1
+            ang(f,mmin-3:m-1) = mg;
+            ang(f,m:mmax+3) = md;
+        elseif mmax > nb_frame-3
+            ang(f,m:end ) = md;
+        elseif mmin <= 3
+            ang(f,1:m-1) = mg;
         end
     end
-end
+    
+    
+    %% correct isolated point
+    l = find(isnan(ind2) == 0);
+    for i = 1:size(l,2)
+        q = ind2(l(i));
+        
+        if q <= nb_frame-4 && q >= 1
+            md = mean(ang(f,q+3:q+4),'omitnan');
+            mg = mean(ang(f,q-4:q-3),'omitnan');
+        elseif q> nb_frame-4
+            md = ang(f,end);
+        elseif q <= 4
+            mg = ang(f,1);
+        end
 
-figure;
-plot(angle*180/pi)
+        % correct angle
+        if q <= nb_frame-3 && q-3 >= 1
+            ang(f,q-2:q-1) = mg;
+            ang(f,q:q+2) = md;
+        elseif q > nb_frame-3
+            ang(f,q+1:end ) = md;
+        elseif q <= 3
+            ang(f,1:q) = mg;
+        end        
+    end
+    % adapt first and last angle
+    ang(f,1) = mean(ang(f,2:5),'omitnan');
+    ang(f,end) = mean(ang(f,end-5:end-2),'omitnan');
+    
+    %% Correction of the 0-360 edge
+    for i = 2:nb_frame
+        d1 = (ang(f,i) - ang(f,i-1))*180/pi;
+        if isnan(d1) == 0
+            ta = angle_per_frame(d1);
+            if abs(ta) <= 150
+                angle(f,i) = angle(f,i-1) + ta*pi/180;
+            else
+                angle(f,i) = angle(f,i-1);
+            end
+        end
+    end
+    %% Angle to OMR
+    ang_OMR(f,:) = angle(f,:) - OMRangle;
+    ang1 = mean(ang_OMR(f,1:5),'omitnan');
+    if ang1 > pi
+        ang_OMR(f,:) = ang_OMR(f,:) - 2*pi;
+    end
+    
+    if fig == 1
+        figure;
+        plot(ang_body(f,:));
+        hold on;
+        plot(angle(f,:));
+        plot(ang_OMR(f,:));
+    end
+% end
--- data_OMR_acoustic.m
+++ data_OMR_acoustic.m
@@ -1,20 +1,25 @@
 function [reaction_time, reaction_time_ms, angle_before, angle_escape,...
     nb_fish_considered, nb_fish_escape] = data_OMR_acoustic(nb_detected_object,...
-    nb_frame, angle_to_OMR, fps)
+    nb_frame, ang_OMR, fps)
+
+frame_acoustic_signal = nb_frame-46;
 
 f_remove = [];
 reaction_time = nan(1,nb_detected_object);
 angle_before = nan(1,nb_detected_object);
 angle_escape = nan(1,nb_detected_object);
+ang1 = nan(1,nb_detected_object);
+d = nan(1,25);
 for f = 1:nb_detected_object
-    b = sum(angle_to_OMR(f,nb_frame-55:nb_frame-30));
+    b = sum(ang_OMR(f,nb_frame-55:nb_frame-30));
     if isnan(b) == 0
-        d(f,:) = diff(angle_to_OMR(f,nb_frame-55:nb_frame-30));
-        b = find(abs(d(f,:))>40*pi/180,1);
+        d = diff(ang_OMR(f,nb_frame-55:nb_frame-20));
+        b = find(abs(d)>40*pi/180,1);
         if isempty(b) == 0
-            reaction_time(1,f) = b + nb_frame - 55;
-            angle_before(1,f) = angle_to_OMR(f,b + nb_frame - 57);
-            angle_escape(1,f) = d(f,b);
+            fr = nb_frame - 55 + b;
+            reaction_time(1,f) = nb_frame - 55 + b;
+            angle_before(1,f) = ang_OMR(f,b + nb_frame - 55);
+            angle_escape(1,f) = d(b);
         end
         
     else
@@ -24,6 +29,15 @@ end
 reaction_time(f_remove) = [];
 angle_before(f_remove) = [];
 angle_escape(f_remove) = [];
-reaction_time_ms  = ((reaction_time - (nb_frame-46))/fps + 0.5/fps)*1000;
+reaction_time_ms  = ((reaction_time - (nb_frame-46))/fps + 0.5/150)*1000;
 nb_fish_considered = size(reaction_time,2);
 nb_fish_escape = size(reaction_time(~isnan(reaction_time)),2);
+
+ang = ang_OMR;
+ang(f_remove,:) = [];
+
+im = -9:1:46;
+im = im*1000/150;
+plot(im,ang(:,nb_frame-55:end));
+hold on;
+plot([0 0],ylim,'k');

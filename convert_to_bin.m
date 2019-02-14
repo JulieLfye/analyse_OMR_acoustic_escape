f7f441c911e4e73421f7d63b0eaccc621cda851e
Julie Lafaye <julie.lafaye07@gmail.com>
2019-02-14 10:52:56 +0100

    update 2 codes

--- convert_to_bin.m
+++ convert_to_bin.m
@@ -3,7 +3,7 @@ clear;
 clc;
 
 disp('Select the first frame');
-[file,path] = uigetfile('*.pgm',[],'C:\Users\LJP\Documents\MATLAB\these\manip_pot_vibrant_data\');
+[file,path] = uigetfile('*.pgm',[],'C:\Users\LJP\Documents\MATLAB\these\data_OMR\');
 n = input('Number of frame to open? ');
 im = imread(fullfile(path,file));
 movie = zeros(size(im,1),size(im,2));
@@ -22,10 +22,12 @@ for i = 1:n
     im = frame_open(file,path,i);
     movie = uint8(frame_process(im)*255);
     
-    c = floor(i/100);
-    d = floor((i-c*100)/10);
-    u = floor(i-c*100-d*10);
+    m = floor(i/1000);
+    c = floor((i-m*1000)/100);
+    d = floor((i-m*1000-c*100)/10);
+    u = floor(i-m*1000-c*100-d*10);
     s = size(im_name,2);
+    im_name(s-7) = num2str(m);
     im_name(s-6) = num2str(c);
     im_name(s-5) = num2str(d);
     im_name(s-4) = num2str(u);
@@ -34,4 +36,5 @@ for i = 1:n
     waitbar(i/n,w);
 end
 close(w);
+close all
 toc
